af6c89979a36c4e9815d7404b8705656074b0aa4
Camila Marques <mfs.cami@gmail.com>
2024-06-01 21:22:45 -0300

    Update Desafio de Código 2 Pontos de Experiencia.js

--- Desafio de Código 2 Pontos de Experiencia.js
+++ Desafio de Código 2 Pontos de Experiencia.js
@@ -1,7 +1,6 @@
 let num1 = parseInt("45");
 let num2 = parseInt("40");
 
-//TODO: Implementar a lógica para exibir o xpGanho:
 const xpGanho = num1 * num2 * 100;
 
 console.log("Voce ganhou " + xpGanho + " XP!");
